0698c30e0448836724ad604f25e5ab12b0d87cd9
Fatime Hashim <fatimemachira@gmail.com>
2024-02-29 09:26:49 +0300

    Image was not implemented

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+void main()
+{
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home:Scaffold(
+      body: Center(
+        child: Text("Elimisha Trust Fund"),
+      ),
+    )
+  ),
+  );
+  FlutterNativeSplash.remove();
+      
+}
